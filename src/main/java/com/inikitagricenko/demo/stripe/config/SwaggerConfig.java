28cb83b139e7d5fbeed60d3857a2f21d2db84da5
iNikitaGricenko <VfVristalt@gmail.com>
2023-07-13 20:17:18 +0300

    move swagger config

--- src/main/java/com/inikitagricenko/demo/stripe/config/SwaggerConfig.java
+++ src/main/java/com/inikitagricenko/demo/stripe/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.inikitagricenko.demo.stripe;
+package com.inikitagricenko.demo.stripe.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
