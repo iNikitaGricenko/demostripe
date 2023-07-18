a27999891211b2fc0319397e8061913653910ff1
iNikitaGricenko <VfVristalt@gmail.com>
2023-07-18 16:37:48 +0300

    retrieve card id

--- src/main/java/com/inikitagricenko/demo/stripe/service/stripe/StripeCardService.java
+++ src/main/java/com/inikitagricenko/demo/stripe/service/stripe/StripeCardService.java
@@ -6,20 +6,18 @@ import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
 import com.stripe.param.PaymentSourceCollectionCreateParams;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class StripeCardService {
 
-	@Async
-	public void addCustomerCard(Customer customer, PaymentMethod paymentMethod) {
+	public String addCustomerCard(Customer customer, PaymentMethod paymentMethod) {
 		try {
 			PaymentSourceCollectionCreateParams paymentSourceCollectionCreateParams = PaymentSourceCollectionCreateParams.builder()
 					.setSource(paymentMethod.getSource())
 					.build();
-			customer.getSources().create(paymentSourceCollectionCreateParams);
+			return customer.getSources().create(paymentSourceCollectionCreateParams).getId();
 		} catch (StripeException e) {
 			throw new DefaultBackendException(e);
 		}
